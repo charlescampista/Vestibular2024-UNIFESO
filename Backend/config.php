a2e5eddf3378269e2cf0fff1c139561a6bc829f4
charlescampista <charlescampista@gmail.com>
2023-09-17 19:54:12 -0300

    Ajustes

--- Backend/config.php
+++ Backend/config.php
@@ -2,13 +2,6 @@
 define('APP_NAME', 'VESTIBULAR UNIFESO 2024');
 define('APP_VERSION', '1.0.0');
 
-//MYSQL
-// define('MYSQL_SERVER', 'localhost');
-// define('MYSQL_DATABASE', 'id21266869_unifeso');
-// define('MYSQL_USER', 'id21266869_unifesoadmin');
-// define('MYSQL_PASSWORD', 'Vestibular###2024');
-//define('MYSQL_CHARSET', 'utf8');
-
 //MYSQL - Preencha abaixo com as informações do seu banco de dados MYSQL
 define('MYSQL_SERVER', '');
 define('MYSQL_DATABASE', '');
